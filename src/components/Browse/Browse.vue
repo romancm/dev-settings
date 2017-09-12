12cbfd3e64c5b84c436299e9a6cd33518e3f89bb
Roman Cervantes <roman.cervantes@infusionsoft.com>
2017-09-12 15:21:28 -0700

    Browse endpoint placeholder

--- src/components/Browse/Browse.vue
+++ src/components/Browse/Browse.vue
@@ -2,11 +2,37 @@
   <div>
       Browse
       List all users here
+
+      <ul>
+          <li v-for="{firstName, lastName, _id} in users">
+              {{firstName}} {{lastName}} {{_id}}
+          </li>
+      </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { store } from '../../store';
+
 export default {
+    mounted() {
+        this.load();
+    },
+
+    computed: {
+        users() { return store.getters.users; },
+    },
+
+    methods: {
+        load() {
+            // TODO: enhance this logic so endpoint isn't hit every time
+            axios.get('https://atom-settings-api.herokuapp.com/browse/')
+                .then(({ data }) => {
+                    store.commit('updateBrowseData', data);
+                });
+        },
+    },
 };
 </script>
 
--- src/store.js
+++ src/store.js
@@ -2,14 +2,16 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import moment from 'moment';
 import axios from 'axios';
-
 import createPersistedState from 'vuex-persistedstate';
+
+// TODO: find more suitable names for methods
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
         user: {},
+        users: {},
         session: {},
         gistData: {},
     },
@@ -24,6 +26,10 @@ export const store = new Vuex.Store({
         updateUser(state, user) {
             console.log('user updated');
             state.user = user;
+        },
+
+        updateBrowseData(state, users) {
+            state.users = users;
         },
 
         updateGistData(state, gistData) {
@@ -52,6 +58,7 @@ export const store = new Vuex.Store({
     getters: {
         session(state) { return state.session; },
         user(state) { return state.user; },
+        users(state) { return state.users; },
         gistData(state) { return state.gistData; },
     },
     plugins: [createPersistedState()],
