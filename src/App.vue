8d7fc10bf2e9eb3a57f4aa72252224cf0665b4ce
Roman Cervantes <roman.cervantes@infusionsoft.com>
2017-10-11 23:28:03 -0700

    Removed verified banner component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
     <main id="app">
         <nav-header/>
-        <verified-banner/>
         <router-view/>
     </main>
 </template>
 
 <script>
-import VerifiedBanner from './components/VerifiedBanner/VerifiedBanner';
 import NavHeader from './components/NavHeader/NavHeader';
 
 export default {
     name: 'app',
 
     components: {
-        VerifiedBanner,
         NavHeader,
     },
 };
