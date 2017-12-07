8b7b74652a653fa65db281c65240fcdff5ec0c7b
Roman Cervantes <roman.cervantes@infusionsoft.com>
2017-12-07 15:52:44 -0700

    Getting started wip

--- src/pages/Settings/SettingsGetStarted.vue
+++ src/pages/Settings/SettingsGetStarted.vue
@@ -1,12 +1,32 @@
 <template lang="html">
     <div class="col-xs-12">
-        <!-- <h3>Hello, {{session.github.login}}!</h3> -->
-        <!-- <p>AtomSettings helps developers around to world to share and discover settings for the hackable text editor for the 21st Century. In order to get started you need to install Sync Settings for Atom. Please refer to their readme for instructions on how to set it up. Once you are all set you can enter the gist id.</p> -->
+        <!-- <el-button style="margin-top: 12px;" @click="next">Next step</el-button>
+        <el-steps align-center :active="active" finish-status="success">
+            <el-step title="Install SyncSettings" />
+            <el-step title="Processing" />
+            <el-step title="Step 3" />
+        </el-steps> -->
+
+
+        <!-- <div v-if="active === 0">
+            <h1>Install SyncSettings package</h1>
+            <pre>apm install sync-settings or using the Install packages pane from Atom Settings.</pre>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <vue-markdown :source="instructions" v-if="instructions" />
+                </div>
+            </div>
+        </div> -->
+
         <div class="panel panel-default">
             <div class="panel-body">
                 <vue-markdown :source="instructions" v-if="instructions" />
             </div>
         </div>
+
+
+        <!-- <h3>Hello, {{session.github.login}}!</h3> -->
+        <!-- <p>AtomSettings helps developers around to world to share and discover settings for the hackable text editor for the 21st Century. In order to get started you need to install Sync Settings for Atom. Please refer to their readme for instructions on how to set it up. Once you are all set you can enter the gist id.</p> -->
     </div>
 </template>
 
@@ -22,6 +42,7 @@ export default {
     data() {
         return {
             instructions: null,
+            active: 0,
         };
     },
 
@@ -34,6 +55,13 @@ export default {
     },
 
     methods: {
+        next() {
+            this.active += 1;
+            if (this.active > 2) {
+                this.active = 0;
+            }
+        },
+
         getSyncReadme() {
             this.$http.get('https://www.atom.io/api/packages/sync-settings').then(({ data }) => {
                 this.instructions = data.readme;
