a1df3ecb2673e274be655323274e507cd27118d4
Roman Cervantes <roman.cervantes@infusionsoft.com>
2017-12-07 15:43:11 -0700

    Improved logout ui

--- src/pages/Logout/Logout.vue
+++ src/pages/Logout/Logout.vue
@@ -1,29 +1,25 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xs-12">
-                <h3 class="page-header">
-                    <span v-if="sessionExpired">
-                        Your session has expired
+    <el-container>
+        <el-main>
+            <el-row>
+                <el-col :xs="{ span: 20, offset: 1}" :sm="{ span: 12, offset: 6 }">
+                    <h2>
+                        <span v-if="sessionExpired">
+                            Your session has expired
+                        </span>
+                        <span v-else>
+                            You've been successfully logged out
+                        </span>
+                    </h2>
+                    <span v-if="!sessionExpired">
+                        <p>Thank you for spending some quality time with us today!</p>
+                        <p>Didn't mean to log out?</p>
                     </span>
-                    <span v-else>
-                        You've been successfully logged out
-                    </span>
-                </h3>
-                <p v-if="sessionExpired">
-                    You've been automatically logged out.
-                </p>
-                <router-link :to="{ name: 'home' }" class="btn btn-primary">
-                    <i class="fa fa-home" />
-                    Home
-                </router-link>
-                <a :href="'https://github.com/login/oauth/authorize?scope=user:email&client_id=' + environment.githubClientId" class="btn btn-primary">
-                    <i class="fa fa-github" />
-                    Login again
-                </a>
-            </div>
-        </div>
-    </div>
+                    <el-button type="primary" @click="login">Login</el-button>
+                </el-col>
+            </el-row>
+        </el-main>
+    </el-container>
 </template>
 
 <script>
@@ -34,7 +30,9 @@
             session() { return store.getters.session; },
             sessionExpired() { return this.$route.query.sessionExpired; },
             environment() { return store.getters.environment; },
-            // environment() { return store.getters.environment; },
+            githuburl() {
+                return `https://github.com/login/oauth/authorize?scope=user:email&client_id=${this.environment.githubClientId}`;
+            },
         },
 
         mounted() {
@@ -42,5 +40,19 @@
                 store.commit('logout');
             }
         },
+
+        methods: {
+            login() {
+                window.location.href = this.githuburl;
+            },
+        },
     };
 </script>
+
+<style lang="scss" rel="stylesheet/scss">
+    @import "~styles/_variables";
+
+    .success {
+        color: $color-success;
+    }
+</style>
