53ed7f223fc5019c5fe128b8f08ca86cd74ee6de
Roman Cervantes <roman.cervantes@infusionsoft.com>
2017-12-13 11:06:50 -0700

    Homepage adjustments

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,76 +1,34 @@
 <template>
     <div class="home">
         <div class="hero">
-            <h2>Welcome home, developers</h2>
-            <p>From software architects to hobbyist, share and discover atom settings,<br /> discover workspaces, packages and more.</p>
-            <el-button type="primary" v-if="!session.token" @click="login">
-                <i class="fa fa-github" aria-hidden="true"></i>  Login with Github
-            </el-button>
+            <div class="welcome-note">
+                <h2>Welcome home, developers</h2>
+                <p>From software architects to hobbyist, share and discover settings, workspaces, packages and more.</p>
+                <el-button type="primary" v-if="!session.token" @click="login">
+                    <i class="fa fa-github" aria-hidden="true" />
+                    Login with Github
+                </el-button>
+                <el-button type="primary" v-else @click="browse">
+                    <i class="fa fa-search" aria-hidden="true" />
+                    Browse
+                </el-button>
+            </div>
+
+            <div class="features">
+                <el-row :gutter="0">
+                    <el-col :lg="8" :md="12" :sm="12" :xs="12" v-for="({ icon, desc }, name) in features">
+                        <div :class="name" class="feature">
+                            <i class="fa" :class="icon" aria-hidden="true"></i>
+                            <div>
+                                <h3 v-html="name" />
+                                <p v-html="desc" />
+                            </div>
+                        </div>
+                    </el-col>
+                </el-row>
+            </div>
         </div>
 
-        <div class="features">
-            <el-row :gutter="0">
-                <el-col>
-                    <div class="feature title">
-                        <h3>Share and Discover</h3>
-                    </div>
-                </el-col>
-                <el-col :lg="8" :md="12" :sm="12" :xs="12">
-                    <div class="feature settings">
-                        <i class="fa fa-cogs fa-5x" aria-hidden="true"></i>
-                        <div>
-                            <h3>Settings</h3>
-                            <p>Font settings, tab size, scroll speed and more.</p>
-                        </div>
-                    </div>
-                </el-col>
-                <el-col :lg="8" :md="12" :sm="12" :xs="12">
-                    <div class="feature packages">
-                        <i class="fa fa-archive fa-5x" aria-hidden="true"></i>
-                        <div>
-                            <h3>Packages</h3>
-                            <p>Make Atom do amazing things with packages.</p>
-                        </div>
-                    </div>
-                </el-col>
-                <el-col :lg="8" :md="12" :sm="12" :xs="12">
-                    <div class="feature scripts">
-                        <i class="fa fa-terminal fa-5x" aria-hidden="true"></i>
-                        <div>
-                            <h3>Scripts</h3>
-                            <p>Discover and share your favorite flavor of Atom.</p>
-                        </div>
-                    </div>
-                </el-col>
-                <el-col :lg="8" :md="12" :sm="12" :xs="12">
-                    <div class="feature snippets">
-                        <i class="fa fa-code fa-5x" aria-hidden="true"></i>
-                        <div>
-                            <h3>Snippets</h3>
-                            <p>Incredibly powerful way to quickly generate commonly needed code syntax from a shortcut.</p>
-                        </div>
-                    </div>
-                </el-col>
-                <el-col :lg="8" :md="12" :sm="12" :xs="12">
-                    <div class="feature keymaps">
-                        <i class="fa fa-keyboard-o fa-5x" aria-hidden="true"></i>
-                        <div>
-                            <h3>Keymaps</h3>
-                            <p>Associate keystrokes with events in specific contexts.</p>
-                        </div>
-                    </div>
-                </el-col>
-                <el-col :lg="8" :md="12" :sm="12" :xs="12">
-                    <div class="feature styles">
-                        <i class="fa fa-css3 fa-5x" aria-hidden="true"></i>
-                        <div>
-                            <h3>Styles</h3>
-                            <p>Add your own CSS or LESS to fully customize Atom</p>
-                        </div>
-                    </div>
-                </el-col>
-            </el-row>
-        </div>
     </div>
 </template>
 
@@ -83,9 +41,46 @@
             Avatar,
         },
 
+        data() {
+            return {
+                features: {
+                    settings: {
+                        icon: 'fa-cogs',
+                        desc: 'Font settings, tab size, scroll speed and more.',
+                    },
+                    packages: {
+                        icon: 'fa-archive',
+                        desc: 'Make Atom do amazing things with packages.',
+                    },
+                    scripts: {
+                        icon: 'fa-terminal',
+                        desc: 'Discover and share your favorite flavor of Atom.',
+                    },
+                    keymaps: {
+                        icon: 'fa-keyboard-o',
+                        desc: 'Associate keystrokes with events in specific contexts.',
+                    },
+                    snippets: {
+                        icon: 'fa-code',
+                        desc: 'Incredibly powerful way to quickly generate commonly needed code syntax from a shortcut.',
+                    },
+                    styles: {
+                        icon: 'fa-css3',
+                        desc: 'Add your own CSS or LESS to fully customize Atom',
+                    },
+                },
+            };
+        },
+
         computed: {
             session() { return store.getters.session; },
             environment() { return store.getters.environment; },
+            isMobile() {
+                return this.$mq.resize && this.$mq.below(768);
+            },
+            buttonSize() {
+                return this.isMobile ? 'small' : '';
+            },
             githuburl() {
                 return `https://github.com/login/oauth/authorize?scope=user:email&client_id=${this.environment.githubClientId}`;
             },
@@ -94,6 +89,9 @@
         methods: {
             login() {
                 window.location.href = this.githuburl;
+            },
+            browse() {
+                this.$router.push('browse');
             },
         },
     };
@@ -104,74 +102,119 @@
 
     .home {
         margin-top: -60px;
-        height: calc(100vh - 60px);
+        height: 100vh;
         background: url('/static/img/bg-home.jpg') no-repeat top center;
         background-size: cover;
-        overflow: auto;
-        padding: 40px $gp * 2 $gp * 2;
+        display: flex;
+        align-items: center;
+    }
 
-        @media($xs) {
-            background-repeat: repeat-y;
+    @media($mobile-landscape) {
+        .home {
+            height: auto;
+            padding-top: 40px;
         }
     }
 
     .hero {
+        max-width: 600px;
+        margin-left: $gp * 2;
+
+        @media($xs) {
+            margin: $gp * 2;
+            // padding-top: 60px;
+        }
+
+        @media($sm) {
+            margin: $gp * 2;
+            max-width: 100%;
+        }
+
+        h3, h2 {
+            margin: 0;
+        }
+    }
+
+    .welcome-note {
         background: rgba(127, 79, 141, .9);
         text-align: center;
         padding: $gp * 3 0;
-        margin: $gp * 4 0 0;
         color: $color-white;
 
+        @media($short) {
+            padding: $gp * 2 0;
+        }
+
         @media($xs) {
-            margin-top: $gp * 2;
+            padding: $gp;
+
+            h2 { font-size: 18px; }
+            p { font-size: 13px; }
         }
 
-        h2 {
-            margin: 0;
+        @media($md) {
+            padding: $gp * 2;
         }
     }
 
-    .features, .hero {
-        max-width: 600px;
-    }
-
-    .feature {
+    .features {
         background: rgba(255, 255, 255, 0.95);
-        padding: 0 $gp;
-        text-align: center;
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        height: 200px;
+        padding: $gp;
 
-        h3, p {
-            padding: 0;
-            margin: 0;
-        }
-
-        p {
-            font-size: 14px;
-        }
-
-        &.settings { color: #3FB8AF; }
-        &.packages { color: #7F4F8D; }
-        &.scripts { color: #FF6E3A; }
-        &.snippets { color: #FF9E9D; }
-        &.keymaps { color: #FF3D7F; }
-        &.styles { color: #692139; }
-
-        &.title {
-            padding: 0;
+        .feature {
+            padding: $gp $gp * 2;
             text-align: center;
-            height: auto;
+
             @media($xs) {
-                // height: 40px;
+                padding: $gp $gp 0;
             }
+
+            @media($md) {
+                display: flex;
+                text-align: left;
+            }
+
+            i {
+                margin-bottom: $gp;
+                font-size: 60px;
+                @media($short) {
+                    font-size: 44px;
+                }
+
+                @media($xs) {
+                    margin-right: $gp;
+                }
+
+                @media($md) {
+                    margin-right: $gp * 2;
+                }
+            }
+
             h3 {
-                color: #7F4F8D;
-                margin: $gp * 2 0;
-                width: 100%;
+                text-transform: capitalize;
+
+                @media($xs) {
+                    font-size: 15px;
+                }
             }
+
+            p {
+                padding: 0;
+                margin: 0;
+                font-size: 14px;
+
+                @media($xs) {
+                    display: none;
+                }
+            }
+
+            &.settings { color: $color-settings; }
+            &.packages { color: $color-packages; }
+            &.scripts { color: $color-scripts; }
+            &.snippets { color: $color-snippets; }
+            &.keymaps { color: $color-keymaps; }
+            &.styles { color: $color-styles; }
         }
     }
+
 </style>
