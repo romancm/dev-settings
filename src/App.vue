9eae7d820e02a2e1af0ccf2f475b1413f88c6560
Roman Cervantes <roman.cervantes@infusionsoft.com>
2018-01-10 16:25:34 -0700

    Added typography and elementui overrides

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,8 @@ export default {
 <style lang="scss" rel="stylesheet/scss">
     // @import "styles/lumen";
     @import "styles/_variables";
+    @import "styles/_typography";
+    @import "styles/_elementui";
     @import "styles/_global";
 
     .el-footer {
