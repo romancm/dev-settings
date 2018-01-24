e4c098727cb75bedc48dfa46e8de631bf5bfae4b
Roman Cervantes <roman.cervantes@infusionsoft.com>
2018-01-24 10:10:46 -0700

    Add heroku deploy back

---
+++ heroku-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+yarn build
+echo "Build completed"
+echo "Removing old dist files and folder"
+rm -rf ../atom-settings-prod/static
+rm -rf ../atom-settings-prod/index.html
+rm -rf ../atom-settings-prod/service-worker.js
+echo "Moving new dist files and folder"
+mv dist/* ../atom-settings-prod/
+echo "Pushing files to Heroku"
+cd ../atom-settings-prod
+git add .
+git commit -am "Deploying"
+git push heroku master
